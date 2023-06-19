55c7c9b53d98e5cd27b1d72667acb5bde8448c6c
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-06-19 17:05:57 +0700

    :tada: feat: init project #1

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/sivaosorg/postgresconn
+
+go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/sivaosorg/govm v1.0.3
+)
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ postgresconn/postgresconn.go
@@ -0,0 +1,73 @@
+package postgresconn
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/sivaosorg/govm/callback"
+	"github.com/sivaosorg/govm/common"
+	"github.com/sivaosorg/govm/dbx"
+	"github.com/sivaosorg/govm/logger"
+	"github.com/sivaosorg/govm/postgres"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	instance *sqlx.DB
+	_logger  = logger.NewLogger()
+)
+
+func NewClient(config postgres.PostgresConfig) (*sqlx.DB, dbx.Dbx) {
+	s := dbx.NewDbx().SetDatabase(config.Database)
+	if !config.IsEnabled {
+		s.SetConnected(false).
+			SetMessage("Postgres unavailable").
+			SetError(fmt.Errorf(s.Message))
+		return &sqlx.DB{}, *s
+	}
+	if instance != nil {
+		s.SetConnected(true)
+		return instance, *s
+	}
+	stringConn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
+		config.Host, config.Port, config.Username, config.Database, config.Password, config.SSLMode)
+	if config.DebugMode {
+		_logger.Info(fmt.Sprintf("Postgres client connection:: %s", stringConn))
+	}
+	client, err := sqlx.Open(common.EntryKeyPostgres, stringConn)
+	if err != nil {
+		s.SetError(err).SetConnected(false).SetMessage(err.Error())
+		return nil, *s
+	}
+	err = client.Ping()
+	if err != nil {
+		s.SetError(err).SetConnected(false).SetMessage(err.Error())
+		return nil, *s
+	}
+	if config.DebugMode {
+		_logger.Info(fmt.Sprintf("Connected successfully to postgres database %s:%d/%s", config.Host, config.Port, config.Database))
+	}
+	client.SetMaxIdleConns(config.MaxIdleConn)
+	client.SetMaxOpenConns(config.MaxOpenConn)
+	instance = client
+	if config.DebugMode {
+		callback.MeasureTime(func() {
+			pid, err := GetPostgresPIDConn(instance)
+			if err == nil {
+				_logger.Info("Postgres client connection PID:: %d", pid)
+			}
+		})
+	}
+	s.SetConnected(true).SetMessage("Connection established")
+	return instance, *s
+}
+
+func GetPostgresPIDConn(db *sqlx.DB) (int, error) {
+	var pid int
+	err := db.QueryRow("SELECT pg_backend_pid() AS pid").Scan(&pid)
+	if err != nil {
+		return 0, err
+	}
+	return pid, nil
+}
